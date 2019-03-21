c9dfed32895341bfdcb8071ccd7127077c60e492
André Calero Valdez <andrecalerovaldez@gmail.com>
2019-03-21 15:56:44 +0100

    initial version

--- install_libraries.R
+++ install_libraries.R
@@ -1,14 +1,23 @@
-# Installation der benötigten Libraries
-
+# Install all required libraries
 install.packages(c("tidyverse", "Hmisc", "apastats", "plotly", "knitr", 
                    "devtools", "purrr", "forcats", "glue", "googledrive",
                    "haven", "here", "apaTables", "xTable", "sjlabelled", 
                    "ggthemes", "esquisse", "ggvis", "sjmisc", "likert"))
 
+# Papaja
+# Install devtools package if necessary
+if(!"devtools" %in% rownames(installed.packages())) install.packages("devtools")
 
+# Install the stable development verions from GitHub
+devtools::install_github("crsh/papaja")
 
+# apa 
+devtools::install_github("dgromer/apa")
 
-library(devtools)
-devtools::install_github("HCIC/r-tools")
+#apaStats
+devtools::install_github('achetverikov/apastats',subdir='apastats')
+devtools::install_github("dstanley4/apaTables")
 
-hcictools::robot_care
+install.packages("sigr")
+
+install.packages("citr")
