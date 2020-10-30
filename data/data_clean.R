d18fddebc1d8bf24be522406e8663e071434eccb
André Calero Valdez <andrecalerovaldez@gmail.com>
2020-10-30 14:59:38 +0100

    updated to work with template

--- data/data_clean.R
+++ data/data_clean.R
@@ -1,2 +1,12 @@
 # This file changes the raw data into a usable format
+library(sjlabelled)
+
+
+cleaned <- raw %>% 
+  mutate_at(c("nationality", "awareness"),  sjlabelled::as_factor)
+
+
+
+cleaned$nationality
+cleaned$awareness
 
