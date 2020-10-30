2c00effc52c15c6d5eff23cf883706114bb786b5
André Calero Valdez <andrecalerovaldez@gmail.com>
2020-10-30 15:05:22 +0100

    fixed bug

--- data/data_clean.R
+++ data/data_clean.R
@@ -2,11 +2,31 @@
 library(sjlabelled)
 
 
-cleaned <- raw %>% 
+cleaned1 <- raw %>% 
   mutate_at(c("nationality", "awareness"),  sjlabelled::as_factor)
 
 
 
-cleaned$nationality
-cleaned$awareness
+# Change everything to factors
+
+cleaned2 <- cleaned1 %>%  rename(b5_o1n = personality_01,
+         b5_a1 = personality_02)
+
+
+# rename variable to match scales
+
+
+
+# helper function to read labels
+cleaned %>% select(starts_with("personality")) %>% map(get_label)
+
+
+
+# identify parties of countries with political alignment for all countries
+
+
+
+#save file
+
+cleaned <- cleaned2
 
